7b8974b215c385e999d5c4ef9478b1e49c09c84e
Samson Patrick Awili-Jandara <s.awili@pm.me>
2025-09-19 08:38:34 +1000

    New cisco_configuration agent and tasks; small changes to crew.py
    cisco_configuration agent provides cisco switch and router commands + a
    report of its actions saved to ./output. The switch and router commands
    are also in separate text files so that they can be reviewed and
    directly copied and pasted into a console.
    
    Changes to crew.py to accomodate this + agent decorators paired with
    tasks for ease of reading.

--- src/ids3000/crew.py
+++ src/ids3000/crew.py
@@ -20,12 +20,24 @@ class Ids3000():
             config=self.agents_config['suricata_analyst'], # type: ignore[index]
             verbose=False
         )
+    
+    @task
+    def suricata_report(self) -> Task:
+        return Task(
+            config=self.tasks_config['suricata_report'], # type: ignore[index]
+        )
 
     @agent
     def incident_manager(self) -> Agent:
         return Agent(
             config=self.agents_config['incident_manager'], # type: ignore[index]
             verbose=False
+        )
+
+    @task
+    def incident_planning(self) -> Task:
+        return Task(
+            config=self.tasks_config['incident_planning'], # type: ignore[index]
         )
 
     @agent
@@ -37,21 +49,34 @@ class Ids3000():
         )
     
     @task
-    def suricata_report(self) -> Task:
-        return Task(
-            config=self.tasks_config['suricata_report'], # type: ignore[index]
-        )
-
-    @task
-    def incident_planning(self) -> Task:
-        return Task(
-            config=self.tasks_config['incident_planning'], # type: ignore[index]
-        )
-    
-    @task
     def email_task(self) -> Task:
         return Task(
             config=self.tasks_config['email_task'],
+        )
+
+    @agent
+    def cisco_admin(self) -> Agent:
+        return Agent(
+            config=self.agents_config['cisco_admin'], # type: ignore[index]
+            verbose=False
+        )
+
+    @task
+    def cisco_configuration_report(self) -> Task:
+        return Task(
+            config=self.tasks_config['cisco_configuration_report'], # type: ignore[index]
+        )
+
+    @task
+    def supply_router_commands(self) -> Task:
+        return Task(
+            config=self.tasks_config['supply_router_commands'], # type: ignore[index]
+        )
+
+    @task
+    def supply_switch_commands(self) -> Task:
+        return Task(
+            config=self.tasks_config['supply_switch_commands'], # type: ignore[index]
         )
 
     @crew
