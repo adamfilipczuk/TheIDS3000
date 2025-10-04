882e39fa33958094e8f6742d12ffa14aa623dd77
adamfilipczuk <adamflippa@gmail.com>
2025-10-04 23:54:51 +1000

    Added dynamic socket buffer size

--- src/ids3000/socket_listener.py
+++ src/ids3000/socket_listener.py
@@ -12,6 +12,21 @@ DELAY = 5  # seconds of inactivity before sending data
 LOOPS = 3  # number of loops to try to connect before dropping data
 RETRIES = 5  # number of retries per loop
 
+def recv_all(conn, buffer_size=4096):
+    data= b""
+    conn.settimeout(0.5)  # Set a timeout for blocking socket operations
+    while True:
+        try:
+            chunk = conn.recv(buffer_size)
+            if not chunk:
+                break
+            data += chunk
+            if len(chunk) < buffer_size:
+                break
+        except socket.timeout:
+            break
+        return data
+    
 def socket_listener():
     global data_queue
     tries = 0
@@ -26,7 +41,7 @@ def socket_listener():
                     conn, addr = s.accept()
                     with conn:
                         print(f"Connected by {addr}")
-                        data = conn.recv(10000)  # Adjust buffer size as needed can be changed to adjust dynamically
+                        data = recv_all(conn)  # calls function to receive all data dynamically
                         if data:
                             # commented out this printing as it shows in crewai run
                             #print(f"[NEW DATA] {data.decode('utf-8')}")
